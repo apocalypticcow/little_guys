98a6705fa3545501a0cf16118d6b923efa1a9a0b
Lam Long Vu <longvulam91@gmail.com>
2020-11-04 03:53:24 +0100

    Changed name of top-bar container id. Done with top bar template.

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,16 @@
-
 function build() {
-    let body = $("body");
-    
-    $("#topBar").load("top_bar.html");
-
+    $("#topBar-container").load("top_bar.html",
+        (res) => attachEvent("submit", "searchBar", onSubmitted));
 }
 
+function onSubmitted(event) {
+    console.log("submitted");
+}
 
+function attachEvent(eventName, elementid, func) {
+    let element = document.getElementById(elementid);
+    element.addEventListener(eventName, func);
+}
 
 
 $(document).ready(build);

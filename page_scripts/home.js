9d6025ce32595594792f3a68a56ed9101957fc91
Connor Johns <connor.johns97@gmail.com>
2020-11-29 20:05:42 -0800

    added button with link to details page. bit of an important oversight there.

--- page_scripts/home.js
+++ page_scripts/home.js
@@ -13,6 +13,7 @@ function renderBusiness(doc){
     let address = document.createElement("span");
     let city = document.createElement("span");
     let province = document.createElement("span");
+    let a = document.createElement("a");
 
 
     li.setAttribute("data-id", doc.id);
@@ -21,22 +22,28 @@ function renderBusiness(doc){
     address.textContent = doc.data().address;
     city.textContent = doc.data().city;
     province.textContent = doc.data().province;
+    a.href = "./details.html";
+    a.textContent = "Details";
+    
 
     li.appendChild(name);
     li.appendChild(phone);
     li.appendChild(address);
     li.appendChild(city);
     li.appendChild(province);
+    li.appendChild(a);
 
     businessList.appendChild(li);
 }
 
+// Search button functionality
 form.addEventListener("submit", (e) =>{
     e.preventDefault();
     input = form.searchBar.value;
     orderList(input);
 })
 
+// Resets list items, displays only specified items.
 function orderList(orderBy){
     $("#business-list").empty();
     db.collection("businesses").where("category", "==", orderBy).get().then((snapshot) =>{
@@ -46,6 +53,7 @@ function orderList(orderBy){
     })
 }
 
+// Display all businesses
 db.collection("businesses").get().then((snapshot) =>{
     snapshot.docs.forEach(doc => {
         renderBusiness(doc);
